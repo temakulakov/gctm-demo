481f5b49b27dd736b97714c759287eb0e13e05d1
Artem Kulakov <109291017+temakulakov@users.noreply.github.com>
2024-03-03 10:45:32 +0300

    init

--- src/components/Header.first.tsx
+++ src/components/Header.first.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {AnimatePresence, motion} from "framer-motion"
 import styles from "styles/Header.first.module.scss";
 import logoWhite from "../uploads/logo-white.svg";
@@ -11,6 +11,8 @@ const HeaderFirst = () => {
     const [ fullWidth, setFullWidth ] = useState(true);
     const { scrollY, direction} = useScrollPosition();
     const [ selectedItem, setSelectedItem ] = useState<INavElemenet | null>(null);
+
+    const bottomRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         if ( scrollY < 400 ) {
@@ -36,6 +38,7 @@ const HeaderFirst = () => {
     }, [scrollY, direction]);
 
     return <>
+
         <motion.header className={styles.root}>
             <motion.div className={styles.header}>
                 <motion.a>
@@ -48,11 +51,11 @@ const HeaderFirst = () => {
                 </motion.div>
             </motion.div>
             <motion.div
+                onMouseLeave={(e) => setSelectedItem(null)}
                 className={styles.menu}>
                 {
                     navElemenet.map((item, index) => <motion.a
-                        onMouseLeave={(e) => setSelectedItem(null)}
-                        onMouseEnter={() => item.subtitle ? setSelectedItem(item) : null}
+                        onMouseEnter={() => item.subtitle ? setSelectedItem(item) : setSelectedItem(null)}
                         href={item.link}
                         key={index}
                     >
@@ -60,20 +63,34 @@ const HeaderFirst = () => {
                     </motion.a>)
                 }
             </motion.div>
-            <AnimatePresence>
-
-            {selectedItem && <motion.div
-                initial={{opacity: 0, width: "97%", y: 0}}
-                animate={{opacity: 1, width: "100%", y: 30}}
-                exit={{ opacity: 0, width: "97%", y: 0 }}
-                className={styles.panel}
-                onMouseLeave={() => setSelectedItem(null)}>
-                {
-                    selectedItem && selectedItem.title
-                }
-            </motion.div>}
-            </AnimatePresence>
-
+                <AnimatePresence>
+                    {selectedItem && <motion.div
+                        ref={bottomRef}
+                        key={selectedItem.title}
+                        initial={{opacity: 0, width: "97%", y: 0}}
+                        animate={{opacity: 1, width: "100%", y: 20}}
+                        exit={{opacity: 0, width: "97%", y: 0}}
+                        className={styles.panel}
+                        onMouseLeave={(e) => {
+                            const target = e.target as Node;
+                            if (bottomRef.current && !bottomRef.current.contains(target)) {
+                                setSelectedItem(null);
+                            }
+                        }}
+                        >
+                        <AnimatePresence>
+                            {selectedItem && <motion.ol
+                                initial={{opacity: 0}}
+                                animate={{opacity: 1}}
+                                exit={{opacity: 0}}
+                            >
+                                {
+                                    selectedItem.subtitle?.map((item, index) => <li key={index}>{item.title}</li>)
+                                }
+                            </motion.ol>}
+                        </AnimatePresence>
+                    </motion.div>}
+                </AnimatePresence>
         </motion.header>
     </>
 };
